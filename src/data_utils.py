05e5d16e7b841b1aed69882da180197bed0867cb
Ziao Huang <71763696+amazing-fish@users.noreply.github.com>
2023-03-16 23:43:10 +0800

    Update data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -2,6 +2,8 @@ import os
 import librosa
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+
 
 def extract_mfcc(file_path, n_mfcc=13):
     y, sr = librosa.load(file_path, sr=None)
@@ -13,6 +15,8 @@ def extract_mfcc(file_path, n_mfcc=13):
     mfcc = scaler.fit_transform(mfcc)
 
     return mfcc
+
+
 def process_audio_files(src_path, dst_path, dataset_type):
     for root, dirs, files in os.walk(src_path):
         for file in files:
@@ -24,6 +28,7 @@ def process_audio_files(src_path, dst_path, dataset_type):
                 dest_folder = os.path.join(dst_path, speaker_id)
                 os.makedirs(dest_folder, exist_ok=True)
                 np.save(os.path.join(dest_folder, file_id + '.npy'), mfcc)
+
 
 def process_transcripts(src_path, dst_path, dataset_type):
     for root, dirs, files in os.walk(src_path):
@@ -41,6 +46,7 @@ def process_transcripts(src_path, dst_path, dataset_type):
                         with open(dest_file_path, 'w') as dest_file:
                             dest_file.write(text)
 
+
 def preprocess_librispeech(src_path, dst_path):
     for dataset_type in ['train', 'dev', 'test']:
         process_audio_files(os.path.join(src_path, dataset_type),
@@ -49,6 +55,49 @@ def preprocess_librispeech(src_path, dst_path):
         process_transcripts(os.path.join(src_path, dataset_type),
                             os.path.join(dst_path, dataset_type),
                             dataset_type)
+
+
+def load_data(data_path):
+    train_data, dev_data, test_data = [], [], []
+    for dataset_type in ['train', 'dev', 'test']:
+        audio_data = []
+        transcript_data = []
+        dataset_path = os.path.join(data_path, dataset_type)
+        for speaker_id in os.listdir(dataset_path):
+            speaker_path = os.path.join(dataset_path, speaker_id)
+            for file in os.listdir(speaker_path):
+                file_path = os.path.join(speaker_path, file)
+                if file.endswith('.npy'):
+                    audio_data.append(np.load(file_path))
+                elif file.endswith('.txt'):
+                    with open(file_path, 'r') as transcript_file:
+                        transcript_data.append(transcript_file.read().strip())
+
+        if dataset_type == 'train':
+            train_data = (audio_data, transcript_data)
+        elif dataset_type == 'dev':
+            dev_data = (audio_data, transcript_data)
+        elif dataset_type == 'test':
+            test_data = (audio_data, transcript_data)
+
+    return train_data, dev_data, test_data
+
+
+def pad_sequence(sequences, max_length, padding_value=0):
+    return np.array(
+        [np.pad(seq, (0, max_length - len(seq)), mode='constant', constant_values=padding_value) for seq in sequences])
+
+
+def create_dataset(data, batch_size):
+    audio_data, transcript_data = data
+    padded_audio_data = pad_sequence(audio_data, max_length=max([len(seq) for seq in audio_data]))
+    padded_transcript_data = pad_sequence(transcript_data, max_length=max([len(seq) for seq in transcript_data]))
+
+    dataset = tf.data.Dataset.from_tensor_slices((padded_audio_data, padded_transcript_data))
+    dataset = dataset.shuffle(buffer_size=len(audio_data))
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+
+    return dataset
 
 
 if __name__ == "__main__":
