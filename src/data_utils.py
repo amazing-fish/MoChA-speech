ce75de166de7c2c1668dfdee6330cee8c8294acd
Ziao Huang <71763696+amazing-fish@users.noreply.github.com>
2023-03-18 00:52:47 +0800

    Update data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,50 +1,10 @@
 import os
-import librosa
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
+from tqdm import tqdm
 
-
-def extract_mfcc(file_path, n_mfcc=13):
-    y, sr = librosa.load(file_path, sr=None)
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
-    mfcc = mfcc.T
-
-    # Standardize MFCC features
-    scaler = StandardScaler()
-    mfcc = scaler.fit_transform(mfcc)
-
-    return mfcc
-
-
-def process_audio_files(src_path, dst_path, dataset_type):
-    for root, dirs, files in os.walk(src_path):
-        for file in files:
-            if file.endswith('.flac'):
-                file_path = os.path.join(root, file)
-                mfcc = extract_mfcc(file_path)
-                file_id = file[:-5]
-                speaker_id = root.split(os.path.sep)[-2]
-                dest_folder = os.path.join(dst_path, speaker_id)
-                os.makedirs(dest_folder, exist_ok=True)
-                np.save(os.path.join(dest_folder, file_id + '.npy'), mfcc)
-
-
-def process_transcripts(src_path, dst_path, dataset_type):
-    for root, dirs, files in os.walk(src_path):
-        for file in files:
-            if file.endswith('.txt'):
-                file_path = os.path.join(root, file)
-                speaker_id = root.split(os.path.sep)[-2]
-                dest_folder = os.path.join(dst_path, speaker_id)
-                os.makedirs(dest_folder, exist_ok=True)
-                with open(file_path, 'r') as transcript_file:
-                    lines = transcript_file.readlines()
-                    for line in lines:
-                        file_id, text = line.strip().split(' ', 1)
-                        dest_file_path = os.path.join(dest_folder, file_id + '.txt')
-                        with open(dest_file_path, 'w') as dest_file:
-                            dest_file.write(text)
+from audio_utils import process_audio_files
+from txt_utils import process_transcripts, text_to_int_sequence
 
 
 def preprocess_librispeech(src_path, dst_path):
@@ -57,53 +17,69 @@ def preprocess_librispeech(src_path, dst_path):
                             dataset_type)
 
 
+def data_generator(file_pattern):
+    def parse_example(audio_file_path, transcript_file_path):
+        audio_data = np.load(audio_file_path.numpy())
+        with open(transcript_file_path.numpy(), 'r') as transcript_file:
+            transcript_data = text_to_int_sequence(transcript_file.read().strip())
+        return audio_data, transcript_data
+
+    def tf_parse_example(audio_file_path, transcript_file_path):
+        audio_data, transcript_data = tf.py_function(
+            parse_example, [audio_file_path, transcript_file_path],
+            [tf.float32, tf.int32])
+        return audio_data, transcript_data
+
+    file_pattern = os.path.join(file_pattern, '*/*.[nt][px][yt]')
+    file_list = tf.data.Dataset.list_files(file_pattern, shuffle=True)
+    audio_files = file_list.filter(lambda x: tf.strings.regex_full_match(x, '.*\.npy'))
+    transcript_files = file_list.filter(lambda x: tf.strings.regex_full_match(x, '.*\.txt'))
+
+    dataset = tf.data.Dataset.zip((audio_files, transcript_files))
+    dataset = dataset.map(tf_parse_example)
+    return dataset
+
+
+def count_examples(file_pattern):
+    count = 0
+    for file in tf.io.gfile.glob(file_pattern):
+        if file.endswith(".npy"):
+            count += 1
+    return count
+
 def load_data(data_path):
-    train_data, dev_data, test_data = [], [], []
-    for dataset_type in ['train', 'dev', 'test']:
-        audio_data = []
-        transcript_data = []
-        dataset_path = os.path.join(data_path, dataset_type)
-        for speaker_id in os.listdir(dataset_path):
-            speaker_path = os.path.join(dataset_path, speaker_id)
-            for file in os.listdir(speaker_path):
-                file_path = os.path.join(speaker_path, file)
-                if file.endswith('.npy'):
-                    audio_data.append(np.load(file_path))
-                elif file.endswith('.txt'):
-                    with open(file_path, 'r') as transcript_file:
-                        transcript_data.append(transcript_file.read().strip())
+    def tqdm_generator(dataset, total_size):
+        return tqdm(dataset, desc="Loading data", total=total_size, unit="example")
 
-        if dataset_type == 'train':
-            train_data = (audio_data, transcript_data)
-        elif dataset_type == 'dev':
-            dev_data = (audio_data, transcript_data)
-        elif dataset_type == 'test':
-            test_data = (audio_data, transcript_data)
+    train_data = data_generator(os.path.join(data_path, 'train'))
+    dev_data = data_generator(os.path.join(data_path, 'dev'))
+    test_data = data_generator(os.path.join(data_path, 'test'))
 
-    return train_data, dev_data, test_data
+    train_size = count_examples(os.path.join(data_path, 'train/*/*.[nt][px][yt]'))
+    dev_size = count_examples(os.path.join(data_path, 'dev/*/*.[nt][px][yt]'))
+    test_size = count_examples(os.path.join(data_path, 'test/*/*.[nt][px][yt]'))
+
+    return (tqdm_generator(train_data, train_size),
+            tqdm_generator(dev_data, dev_size),
+            tqdm_generator(test_data, test_size))
 
 
-def pad_sequence(sequences, max_length=None, padding_value=0):
+def pad_sequence(sequences, max_length=None, padding_value=0, pad_right=True):
     if max_length is None:
         max_length = max([len(seq) for seq in sequences])
 
     padded_sequences = []
     for seq in sequences:
-        padded_seq = np.pad(seq, ((0, max_length - len(seq)), (0, 0)), mode='constant', constant_values=padding_value)
+        padding = (0, max_length - len(seq)) if pad_right else (max_length - len(seq), 0)
+        padded_seq = np.pad(seq, padding, mode='constant', constant_values=padding_value)
         padded_sequences.append(padded_seq)
 
-    return np.array(padded_sequences, dtype=object)
+    return padded_sequences
 
 
-def create_dataset(data, batch_size):
-    audio_data, transcript_data = data
-    padded_audio_data = pad_sequence(audio_data, max_length=max([len(seq) for seq in audio_data]))
-    padded_transcript_data = pad_sequence(transcript_data, max_length=max([len(seq) for seq in transcript_data]))
-
-    dataset = tf.data.Dataset.from_tensor_slices((padded_audio_data, padded_transcript_data))
-    dataset = dataset.shuffle(buffer_size=len(audio_data))
-    dataset = dataset.batch(batch_size, drop_remainder=True)
-
+def create_dataset(data_generator, batch_size):
+    dataset = data_generator.shuffle(buffer_size=1000)
+    dataset = dataset.padded_batch(batch_size, padded_shapes=((None, None), (None,)), drop_remainder=True)
     return dataset
 
 
