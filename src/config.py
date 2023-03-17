669a8dad6f23c7816c83d33c3c51cbf90e14b7b5
Ziao Huang <71763696+amazing-fish@users.noreply.github.com>
2023-03-18 00:52:41 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,34 +1,40 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 class Config:
-    # Data
-    data_path = r'E:\Project\Python\MoChA-speech\data\preprocessed_data'
-    train_data = 'train'
-    dev_data = 'dev'
-    test_data = 'test'
+    @staticmethod
+    def get_config():
+        config = {
+            # Data
+            'data_path': os.path.join(PROJECT_ROOT, 'data', 'preprocessed_data'),
+            'train_data': 'train',
+            'dev_data': 'dev',
+            'test_data': 'test',
 
-    # Audio features
-    input_dim = 13  # Number of MFCC features
-    output_dim = 29  # Number of output classes (characters, including a special 'blank' character)
+            # Audio features
+            'input_dim': 8,
+            'output_dim': 29,
 
-    # Model architecture
-    hidden_dim = 256  # Hidden units in RNN layers
-    num_layers = 2  # Number of RNN layers in both encoder and decoder
-    chunk_size = 8  # MoChA attention chunk size
-    dropout_rate = 0.1  # Dropout rate for RNN layers
+            # Model architecture
+            'hidden_dim': 256,
+            'num_layers': 2,
+            'chunk_size': 8,
+            'dropout_rate': 0.1,
 
-    # Training
-    batch_size = 64
-    num_epochs = 50
-    learning_rate = 0.001
-    clip_gradient_norm = 5.0  # Gradient clipping to avoid exploding gradients
+            # Training
+            'batch_size': 64,
+            'num_epochs': 50,
+            'learning_rate': 0.001,
+            'clip_gradient_norm': 5.0,
 
-    # Evaluation
-    beam_width = 10  # Beam width for beam search during evaluation
+            # Evaluation
+            'beam_width': 10,
 
-    # Checkpoints and logging
-    model_dir = r'E:\Project\Python\MoChA-speech\models\saved_models'
-    log_dir = r'E:\Project\Python\MoChA-speech\logs\training_logs'
+            # Checkpoints and logging
+            'model_dir': os.path.join(PROJECT_ROOT, 'models', 'saved_models'),
+            'log_dir': os.path.join(PROJECT_ROOT, 'logs', 'training_logs')
+        }
 
-    # Character set and mapping
-    char_set = ['<blank>'] + list('abcdefghijklmnopqrstuvwxyz') + [' ']
-    index2char = {index: char for index, char in enumerate(char_set)}
-    char2index = {char: index for index, char in enumerate(char_set)}
+        return config
